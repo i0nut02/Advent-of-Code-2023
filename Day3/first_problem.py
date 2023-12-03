c933c21fea0fcb5829098e0217d8db3542edce59
I0nut02 <ionu.radu02@gmail.com>
2023-12-03 14:39:50 +0100

    add day3 solutions

---
+++ Day3/first_problem.py
@@ -0,0 +1,42 @@
+DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+def search_number(engine, y, x, visited):
+    if not(y >= 0 and y < len(engine) and x >= 0 and x < len(engine[0])):
+        return ""
+
+    if not engine[y][x].isnumeric():
+        return ""
+    
+    if (y, x) in visited:
+        return ""
+    
+    visited.add((y, x))
+    
+    return search_number(engine, y, x -1, visited) + engine[y][x] + search_number(engine, y, x +1, visited)
+
+
+def visit_adjacents(engine, visited, i, j):
+    res = 0
+
+    for dy, dx in DIRECTIONS:
+        ny, nx = i + dy, j + dx
+        if ny >= 0 and ny < len(engine) and nx >= 0 and nx < len(engine[0]):
+            if (ny, nx) not in visited and engine[ny][nx].isnumeric():
+                res += int(search_number(engine, ny, nx, visited))
+    return res
+
+def find_engine_sum(file):
+    engine = None
+
+    with open(file, mode="r") as f:
+        engine = f.read().split("\n")
+    
+    res = 0
+    visited = set()
+    for i in range(len(engine)):
+        for j in range(len(engine[0])):
+            if not engine[i][j].isnumeric() and engine[i][j] != ".":
+                res += visit_adjacents(engine, visited, i, j)
+
+    return res
+
